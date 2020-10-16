ce3084dacae238fe515f2baa5178b6899a3fe3b6
Johannes Kreutz <jkm@jkm-marburg.de>
2020-10-16 18:26:29 +0200

    Add course list

--- selfservice/selfservice/endpoints/courses.py
+++ selfservice/selfservice/endpoints/courses.py
@@ -6,14 +6,27 @@
 
 # Include dependencies
 from flask import Blueprint, request, jsonify
-from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import login_required, current_user
 
 # Include modules
 import modules.database as db
+import modules.permissionCheck as pc
 
 # Endpoint definition
 courseApi = Blueprint("courseApi", __name__)
 @courseApi.route("/api/course/my", methods=["GET"])
 @login_required
 def getMyCourses():
+    dbconn = db.database()
+    dbconn.execute("SELECT G.id, name FROM groups G INNER JOIN people_has_groups PHG ON G.id = PHG.group_id INNER JOIN people P ON P.id = PHG.people_id WHERE P.username = %s AND G.type = 3", (current_user.username,))
+    return jsonify(dbconn.fetchall()), 200
+
+@courseApi.route("/api/course/detail/<id>", methods=["GET"])
+@login_required
+def getCourseDetail(id):
+    dbconn = db.database()
+    pCheck = pc.permissionCheck()
+    if not "grouplst" in pCheck.listForUser(current_user.username):
+        return "ERR_NOT_ALLOWED", 403
+
     return None
