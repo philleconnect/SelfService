a7e594fe87c89c59b9f6feb71d377430a8265fbb
Johannes Kreutz <jkm@jkm-marburg.de>
2020-10-18 14:01:42 +0200

    Check for reset

--- ui/src/js/login.js
+++ ui/src/js/login.js
@@ -17,12 +17,19 @@ let login = {
   show: function() {
     Swal.fire({
       title: 'Bitte Nutzerdaten eingeben.',
-      html: '<input type="text" id="swal-input1" placeholder="Nutzername" class="swal2-input"/><input type="password" id="swal-input2" placeholder="Passwort" class="swal2-input"/>',
+      html: '<input type="text" id="swal-input1" placeholder="Nutzername" class="swal2-input"/><input type="password" id="swal-input2" placeholder="Passwort" class="swal2-input"/><br /><a href=\"#\" onclick=\"\" class=\"nodisplay\" id=\"reset-link\">Passwort vergessen</a>',
       showCancelButton: false,
       confirmButtonText: 'Anmelden',
       showLoaderOnConfirm: true,
       allowOutsideClick: false,
       allowEscapeKey: false,
+      willOpen: function() {
+        api.send("/api/permissions/resetenabled", "GET", {}).then(function(response) {
+          if (response == "ENABLED") {
+            document.getElementById("reset-link").classList.remove("nodisplay");
+          }
+        }.bind(this));
+      }.bind(this),
       preConfirm: function() {
         return new Promise(function(resolve) {
           if (document.getElementById("swal-input1").value == "" || document.getElementById("swal-input2").value == "") {
