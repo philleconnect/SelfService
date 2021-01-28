8efdbe6da1a7cffa90e5972bbda868082d9c7f3b
Johannes Kreutz <jkm@jkm-marburg.de>
2021-01-28 23:58:36 +0100

    Release 1.1.2, enhance compile script

--- createRelease.sh
+++ createRelease.sh
@@ -3,15 +3,28 @@
 # Halt on errors
 set -e
 
+# Paths
+COMPILED_GUI="selfservice/ui"
+ARCHIVE="selfservice.tar.gz"
+CONTAINER_DEST="selfservice/"
+
+# Cleanup
+if [ -d "$COMPILED_GUI" ]; then
+  rm -rf $COMPILED_GUI
+fi
+if [ -f "$ARCHIVE" ]; then
+  rm -rf $ARCHIVE
+fi
+
 # Compile frontend
 pushd ui
 npm install
 npm run build-prod
 popd
 
-# Clear ui folder and copy compiled code over
-rm -rf selfservice/ui/*
-cp -r ui/www/* selfservice/ui/
+# Create ui folder and copy compiled code over
+mkdir $COMPILED_GUI
+cp -r ui/www/* "$COMPILED_GUI/"
 
 # Create archive
-tar cfvz selfservice.tar.gz selfservice/
+tar cfvz $ARCHIVE $CONTAINER_DEST
